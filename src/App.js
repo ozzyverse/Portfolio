3ae80d95455450cb7beec1e2f32a227f5a87f865
ozzyverse <58913474+ozzyverse@users.noreply.github.com>
2024-05-30 23:09:32 +0545

    added cv

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 
 import NavBar from "./components/NavBar";
-import SocialLinks from "./components/SocialLinks";
+import SocialLinks from "./components/SocialLinks.1";
 import Portfolio from "./components/Portfolio";
 import Experience from "./components/Experience";
 import Contact from "./components/Contact";
